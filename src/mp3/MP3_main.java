a35f2be796a19f4caa01e8925982efaef86c9f67
LeeMyeongbo <myeongbo123@naver.com>
2022-04-15 23:24:28 +0900

    .

---
+++ src/mp3/MP3_main.java
@@ -0,0 +1,12 @@
+package mp3;
+
+import javafx.embed.swing.*;
+
+public class MP3_main {
+	public static void main(String[] args) {
+		new JFXPanel(); // Media 클래스가 속한 javafx 는 실행 전에 특수한 초기화 과정을 거쳐야 하는데 이 JFXPanel 이 그 초기화를 해줌
+		new MP3_play();
+		
+		System.out.println("성공했어요.");
+	}
+}
